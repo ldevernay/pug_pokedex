e5ac3dffa33777dde77db56bfc899a4cd07ca95a
ldevernay <boblehamster@yahoo.fr>
2017-05-26 09:19:11 +0200

    node launch

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ var pug = require('pug');
 function datasource(file) {
   return JSON.parse(fs.readFileSync(file, 'utf8'));
 }
-console.log(datasource('pokedex.json'));
 var html = pug.renderFile('pokedex.pug', {datasource: datasource});
 console.log(html);
